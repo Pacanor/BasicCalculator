346f06e7f472d10b0c73d59912c0bff02482a94f
pacanor <papiasb@gmail.com>
2020-01-07 18:40:42 +0200

    task: added calculator with Addition and division

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return Addition.Sum(a, b);
+        }
+
+        public int Divide(int a, int b)
+        {
+            return Division.Quotient(a, b);
+        }
+    }
+}
